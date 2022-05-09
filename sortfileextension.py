728afbfcb354a722aa785939ec98d71c4cf91a93
LiseRochat <liserochat@live.fr>
2022-05-09 21:18:35 +0200

    Update extension in dictionnary

--- sortfileextension.py
+++ sortfileextension.py
@@ -7,14 +7,21 @@ from pathlib import Path
 dirs = {
     ".png" : "Images",
     ".jpeg" : "Images",
-    ".gif" : "Images",
+    ".bmp" : "Images",
+    ".webp" : "Images",
+    ".gif" : "Vidéos",
     ".mp4" : "Vidéos",
+    ".avi" : "Vidéos",
     ".zip" : "Archives",
     ".pdf" : "Documents",
     ".txt" : "Documents",
-    ".json" : "Documents",
+    ".csv" : "Documents",
+    ".xls" : "Documents",
+    ".odp" : "Documents",
+    ".pages" : "Documents",
     ".mp3" : "Musiques",
     ".wav" : "Musiques",
+    ".flac" : "Musiques",
 }
 
 # Recupere le chemin vers mon fichier de tri
@@ -23,7 +30,7 @@ tri_dir = Path.home() / "Tri"
 files = [f for f in tri_dir.iterdir() if f.is_file()]
 for f in files:
     # Concatene mon dossier de tri avec le dossier correspondant en fonction de son extension -> Autres si le fichier n'est pas trouvé
-    output_dir = tri_dir / dirs.get(f.suffix, "Autres")
+    output_dir = tri_dir / dirs.get(f.suffix, "Divers")
     # On créer notre dossier dans lequel on range nos fichiers, on élimine l'erreur si le dossier existe avec exist_ok=True
     output_dir.mkdir(exist_ok=True)
     # On déplace notre fichier d'origine f dans le dossier output f.name : nom de mon fichier
